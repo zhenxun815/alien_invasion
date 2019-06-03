9d7832cf52c7e9efec0e7fbdc511aa16e3f7a596
yiheng <guoyiheng89@gmail.com>
2019-06-04 07:38:54 +0800

    extract bullets control methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,7 +17,7 @@ from pygame.sprite import Group
 
 
 def run_game():
-    """game enter function"""
+    """main function"""
     # init the game and create a screen obj
     pygame.init()
     ai_settings = Settings()
@@ -32,19 +32,9 @@ def run_game():
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
-        delete_bullets(bullets)
+        gf.update_bullets(bullets)
 
         gf.update_screen(ai_settings, screen, ship, bullets)
-
-
-def delete_bullets(bullets):
-    """delete out screen bullets"""
-    for bullet in bullets.copy():
-        if bullet.rect.bottom <= 0:
-            bullets.remove(bullet)
-
-    # print(len(bullets))
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -21,8 +21,9 @@ def check_key_down_events(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         # add one new bullet and add to group
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+        if len(bullets) <= ai_settings.bullets_allowed:
+            new_bullet = Bullet(ai_settings, screen, ship)
+            bullets.add(new_bullet)
 
 
 def check_key_up_events(event, ship):
@@ -52,3 +53,14 @@ def update_screen(ai_settings, screen, ship, bullets):
 
     ship.blitme()
     pygame.display.flip()
+
+
+def update_bullets(bullets):
+    """draw ,move and remove bullets"""
+    bullets.update()
+    # delete out screen bullets
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+    # print(len(bullets))
