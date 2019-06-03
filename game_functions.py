19d8a35e64343f8aa2795ec7ab07f68e07e07136
yiheng <guoyiheng89@gmail.com>
2019-06-04 00:58:13 +0800

    on space key down fire bullet and update screen

--- game_functions.py
+++ game_functions.py
@@ -10,14 +10,19 @@
 
 import sys
 import pygame
+from bullet import Bullet
 
 
-def check_key_down_events(event, ship):
+def check_key_down_events(event, ai_settings, screen, ship, bullets):
     """listen key down events"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # add one new bullet and add to group
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_key_up_events(event, ship):
@@ -28,19 +33,22 @@ def check_key_up_events(event, ship):
         ship.moving_left = False
 
 
-def check_events(ship):
+def check_events(ai_setttings, screen, ship, bullets):
     """listen the keyboard and mouse events"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_key_down_events(event, ship)
+            check_key_down_events(event, ai_setttings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_key_up_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """redraw surfaces and update the display"""
     screen.fill(ai_settings.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
     ship.blitme()
     pygame.display.flip()
