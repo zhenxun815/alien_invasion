49d35de19b86c2d617be6957a3b3237d91567461
yiheng <guoyiheng89@gmail.com>
2019-06-03 08:01:05 +0800

    make ship moving

--- game_functions.py
+++ game_functions.py
@@ -20,12 +20,12 @@ def check_events(ship):
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = True
-            if event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_LEFT:
                 ship.moving_left = True
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = False
-            if event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_LEFT:
                 ship.moving_left = False
 
 
