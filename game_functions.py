21257134174f84661c2bd04bb88220f0956fd47e
yiheng <guoyiheng89@gmail.com>
2019-06-04 19:58:55 +0800

    add exit shortcut

--- game_functions.py
+++ game_functions.py
@@ -21,6 +21,8 @@ def check_key_down_events(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
 
 
 def fire_bullet(ai_settings, screen, ship, bullets):
